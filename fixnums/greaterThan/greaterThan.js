25ad34091eb047bbae6fb894ecd564dc8e36f916
Pete Eichman <peichman14@gmail.com>
2017-02-02 11:49:20 -0500

    added greaterThan

---
+++ fixnums/greaterThan/greaterThan.js
@@ -0,0 +1,5 @@
+//Return the greater number of the two.
+
+function greaterThan(num1, num2) {
+
+}
---
+++ fixnums/greaterThan/greaterThanAnswer.js
@@ -0,0 +1,9 @@
+//Return the greater number of the two.
+
+function greaterThan(num1, num2) {
+ if(num1 > num2) {
+   return num1;
+ } else if(num2 > num1) {
+   return num2;
+ }
+}
